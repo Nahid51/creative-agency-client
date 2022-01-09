f3a44f30969dcf4e5e312492db1637363a2ef154
Nahid51 <hasannahidnahid@gmail.com>
2022-01-10 00:04:25 +0600

    added some logos

--- src/Pages/Home/Logos/Logos.js
+++ src/Pages/Home/Logos/Logos.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import logo1 from '../../../images/logos/airbnb.png';
+import logo2 from '../../../images/logos/google.png';
+import logo3 from '../../../images/logos/netflix.png';
+import logo4 from '../../../images/logos/slack.png';
+import logo5 from '../../../images/logos/uber.png';
 
 const Logos = () => {
     return (
         <div>
-            logos
+            <div style={{ margin: "30px 0" }}>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <Card style={{ width: "7rem", border: "none", marginLeft: "5rem" }} >
+                        <Card.Img variant="top" src={logo1} />
+                    </Card>
+                    <Card style={{ width: "7rem", border: "none", marginLeft: "5rem" }} >
+                        <Card.Img variant="top" src={logo2} />
+                    </Card>
+                    <Card style={{ width: "7rem", border: "none", marginLeft: "5rem" }} >
+                        <Card.Img variant="top" src={logo5} />
+                    </Card>
+                    <Card style={{ width: "7rem", border: "none", marginLeft: "5rem" }} >
+                        <Card.Img variant="top" src={logo3} />
+                    </Card>
+                    <Card style={{ width: "7rem", border: "none", marginLeft: "5rem" }} >
+                        <Card.Img variant="top" src={logo4} />
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 };
