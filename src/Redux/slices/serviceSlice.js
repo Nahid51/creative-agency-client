ac5b7770919ef0a52a002d986efc4d732e1e79ea
Nahid51 <hasannahidnahid@gmail.com>
2022-01-11 01:35:50 +0600

    added login and register system

---
+++ src/Redux/slices/serviceSlice.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const servicesApi = createApi({
+    reducerPath: 'servicesApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5000/'
+    }),
+    endpoints: (builder) => ({
+        getServices: builder.query({
+            query: () => ({
+                url: 'services',
+                method: 'GET'
+            }),
+        }),
+        getAllServices: builder.query({
+            query: () => ({
+                url: 'allservices',
+                method: 'GET'
+            })
+        }),
+        // getProjects: builder.query({
+        //     query: () => ({
+        //         url: 'projects',
+        //         method: 'GET'
+        //     })
+        // }),
+        // getReviews: builder.query({
+        //     query: () => ({
+        //         url: 'reviews',
+        //         method: 'GET'
+        //     })
+        // }),
+        // getOrders: builder.query({
+        //     query: () => ({
+        //         url: 'orderList',
+        //         method: 'GET'
+        //     })
+        // }),
+
+    }),
+})
+
+export const { useGetServicesQuery } = servicesApi
