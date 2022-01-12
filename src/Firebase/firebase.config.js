7355817265f15683be0a3f72a2e79d86109b5454
Nahid51 <hasannahidnahid@gmail.com>
2022-01-12 16:29:56 +0600

    create secret env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDEdAIP6DHISrrrI6uw0YWuMl_J16kFmYU",
-    authDomain: "creative-agency-20caa.firebaseapp.com",
-    projectId: "creative-agency-20caa",
-    storageBucket: "creative-agency-20caa.appspot.com",
-    messagingSenderId: "466880978485",
-    appId: "1:466880978485:web:6a0bbd738defd9658fdb7c"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
